1feb737a4d5ee9e801ae3f1db38df9eaccdcdf6a
umudik <seyitumutbicer@gmail.com>
2023-02-15 19:58:11 +0300

    Desriptions removed.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-//! A "hello world" echo server with Tokio
-//!
-//! This server will create a TCP listener, accept connections in a loop, and
-//! write back everything that's read off of each TCP connection.
-//!
-//! Because the Tokio runtime uses a thread pool, each TCP connection is
-//! processed concurrently with all other TCP connections across multiple
-//! threads.
-//!
-//! To see this server in action, you can run this in one terminal:
-//!
-//!     cargo run --example echo
-//!
-//! and in another terminal you can run:
-//!
-//!     cargo run --example connect 127.0.0.1:8080
-//!
-//! Each line you type in to the `connect` terminal should be echo'd back to
-//! you! If you open up multiple terminals running the `connect` example you
-//! should be able to see them all make progress simultaneously.
-
-#![warn(rust_2018_idioms)]
-
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
@@ -40,12 +17,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         let (mut socket, _) = listener.accept().await?;
         tokio::spawn(async move {
-            let mut buf = vec![0; 1024];
             loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
+                let mut buf = vec![0; 1024];
+                let n = socket.read(&mut buf).await.unwrap();
 
                 if n == 0 {
                     return;
@@ -56,10 +30,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
                 };
 
-                socket
-                    .write_all(&buf[0..n])
-                    .await
-                    .expect("failed to write data to socket");
+                socket.write_all(&buf[0..n]).await.unwrap();
             }
         });
     }
